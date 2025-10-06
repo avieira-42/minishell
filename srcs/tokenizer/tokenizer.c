bf86af31feb870a03919179509a1086a86703c1f
avieira-42 <vieiramendesalex@gmail.com>
2025-10-06 19:23:08 +0100

    tokenizer working and strong | need to test tokenizer thoroughly

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -46,11 +46,11 @@ void    tokenize_quoted_text(t_token_list **tokens, char *user_input, int *i, in
     char            *token_new;
     t_token_list    *token_node_new;
  
-    if (*user_input != DQUOTE_LITERAL && *user_input != SQUOTE_LITERAL) 
+    if (user_input[*i] != DQUOTE_LITERAL && user_input[*i] != SQUOTE_LITERAL)
         return ;
-    if (*user_input == DQUOTE_LITERAL)
+    if (user_input[*i] == DQUOTE_LITERAL)
         tokenize_dquoted_text(tokens, user_input, i);
-    else if (*user_input == SQUOTE_LITERAL)
+    else if (user_input[*i] == SQUOTE_LITERAL)
         tokenize_squoted_text(tokens, user_input, i);
     token_new = ft_substr(user_input, j, *i - j);
     token_node_new = ft_token_lst_new(token_new);
@@ -68,6 +68,11 @@ void    tokenize_operator(char *c, t_token_list **tokens, int *i)
        WITH iteration on double char operator and free on space*/
     t_token_list    *token_node_new;
 
+    if (*c == SPACE)
+    {
+        (*i)++;
+        return ;
+    }
     token_node_new = ft_token_lst_new(NULL);
     ft_token_lst_add_back(tokens, token_node_new);
     if (ft_bool_strncmp(c, HEREDOC, 2) == true)
@@ -80,8 +85,6 @@ void    tokenize_operator(char *c, t_token_list **tokens, int *i)
         token_node_new->token_string = ft_strdup(STRING_REDIRECT_IN);
     else if (*c == REDIRECT_OUT)
         token_node_new->token_string = ft_strdup(STRING_REDIRECT_OUT);
-    else if (*c == SPACE)
-        token_node_new->token_string = ft_strdup(STRING_SPACE);
     /*else if (ft_bool_strncmp(c, LOGICAL_AND, 2) == true)
       (token_node_new->token_type = TOKEN_HEREDOC, (*i)++);
       else if (ft_bool_strncmp(c, LOGICAL_OR, 2) == true)
@@ -96,6 +99,8 @@ void    tokenize_word(t_token_list **tokens, char *user_input, int i, int j)
     char            *token_new;
     t_token_list    *token_node_new;
 
+    if (i == j)
+        return ;
     token_new = ft_substr(user_input, j, i - j);
     token_node_new = ft_token_lst_new(token_new);
     ft_token_lst_add_back(tokens, token_node_new);
@@ -112,19 +117,19 @@ void    tokenize_user_input(t_token_list **tokens, char *user_input)
     {
         if (user_input[i] == SQUOTE_LITERAL || user_input[i] ==DQUOTE_LITERAL)
         {
-            i++;
-            tokenize_quoted_text(tokens, &user_input[j], &i, j);
+            tokenize_quoted_text(tokens, user_input, &i, j);
             j = i;
         }
-        if (is_operator(&user_input[i]) == true)
+        else if (is_operator(&user_input[i]) == true)
         {
             tokenize_word(tokens, user_input, i, j);
             tokenize_operator(&user_input[i], tokens, &i);
-            tokenize_quoted_text(tokens, &user_input[i], &i, i);
+            tokenize_quoted_text(tokens, user_input, &i, i);
             j = i;
         }
         else
             i++;
     }
-    tokenize_word(tokens, user_input, i, j);
+    if (i > j)
+        tokenize_word(tokens, user_input, i, j);
 }

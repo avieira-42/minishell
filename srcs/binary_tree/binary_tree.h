f48ec5b84618087633bc671fe9fedb55fd11e4cc
Julio Cesar Santos Souza <jcesar-s@c2r4s5.42porto.com>
2025-10-15 16:37:18 +0100

    finished structuring the AST data structure for the project

--- srcs/binary_tree/binary_tree.h
+++ srcs/binary_tree/binary_tree.h
@@ -1,11 +1,21 @@
 #ifndef BINARY_TREE_H
 # define BINARY_TREE_H
 
-/* where the binary tree structure of the command line arguments string
- * execution order will be implemented 
- * 
- * currently the idea is to create a strucure so it represents properly the
- * order in which the commands will be eecuted and how, if they are gonna
- * execute alone, if they are gonna execute and pipe to the next cmmd etc..*/
+# include "../tokenizer/tokenizer.h"
+
+typedef struct s_command
+{
+	t_token_type	redir_type;
+	int				redir_count;
+	char			**argv;
+}	t_command;
+
+typedef struct s_btree
+{
+	t_token_type	node_type;
+	t_command		*command;
+	struct s_btree	*left;
+	struct s_btree	*right;
+}	t_btree;
 
 #endif

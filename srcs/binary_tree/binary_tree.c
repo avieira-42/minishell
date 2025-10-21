26fa070e65b85c1f0dd084aef038ea1534f6ce0c
avieira-42 <vieiramendesalex@gmail.com>
2025-10-21 02:27:51 +0100

    figuring out how to fill binary tree

--- srcs/binary_tree/binary_tree.c
+++ srcs/binary_tree/binary_tree.c
@@ -27,13 +27,17 @@ int	command_count(t_token_list *tokens)
 	return (count);
 }
 
-char	**command_get(t_token_list **tokens)
+void	command_init(t_token_list **tokens, t_btree *node)
 {
+	// NEED TO INITIALIZE REDIT_TYPE AND REDIT_COUNT
+	// NEED TO RETHINK THIS WITH JULIO
 	int		i;
 	char	**command;
 
 	i = 0;
 	command = malloc(sizeof(char *) * (command_count(*tokens) + 1));
+	if (command == NULL)
+		exit(1);
 	while (*tokens != NULL && (*tokens)->token_type != TOKEN_PIPE)
 	{
 		if (is_enum_redirect_token((*tokens)->token_type) != false)
@@ -41,25 +45,48 @@ char	**command_get(t_token_list **tokens)
 		i++;
 		(*tokens) = (*tokens)->next;
 	}
-	return (command);
+	node->command->argv = command;
+}
+
+t_btree	*btree_last(t_btree *btree)
+{
+	if (btree == NULL)
+		return (NULL);
+	if (btree->node_type == TOKEN_CMD)
+		return (btree);
+	if (btree->left == NULL)
+		return (btree_last(btree->left));
+	return (btree_last(btree->right));
+}
+
+void	btree_add_back(t_btree *btree, t_btree node_new)
+{
+	t_btree	*node_last;
+
+	node_last = btree_last(btree);
 }
 
 void	btree_create(t_token_list *tokens)
 {
-	t_command		**command_node;
-	t_token_type	token_type;
-	t_btree			*b_tree;
+	t_btree			*btree;
 	t_btree			*node_new;
+	t_btree			*node_last;
 
-	b_tree = NULL;
+	btree = NULL;
 	while (tokens != NULL)
 	{
 		node_new = btree_add_new();
-		command_node = NULL;
 		if (tokens->token_type == TOKEN_CMD)
-			node_new->command = command_get(&tokens);
+		{
+			node_new->node_type = TOKEN_CMD;
+			command_init(&tokens, node_new);
+		}
 		else if (tokens->token_type == TOKEN_PIPE)
-			command = NULL;
+		{
+			node_new->node_type = TOKEN_PIPE;
+			node_new->command = NULL;
+		}
+		btree_add_back(btree, node_new);
 		tokens = tokens->next;
 	}
 }

610335da28b24dd60eb0ed1bc5ab137ecca4a18f
Julio Souza <juliosouza.ux@gmail.com>
2025-11-01 16:22:59 +0000

    added checks in env function

--- srcs/builtins/builtins.h
+++ srcs/builtins/builtins.h
@@ -4,6 +4,6 @@
 int	builtins_echo(char **user_input);
 int	builtins_exec(char **argv);
 int	builtins_pwd(void);
-int	builtins_env(char **envp);
+int	builtins_env(char **argv, char **envp);
 
 #endif

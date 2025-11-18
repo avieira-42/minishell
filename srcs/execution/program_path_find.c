e9ec390436974bfba83f52055cfcf7c78255b136
Julio Souza <juliosouza.ux@gmail.com>
2025-11-18 20:45:08 +0000

    fix: error handling parsing paths

--- srcs/execution/program_path_find.c
+++ srcs/execution/program_path_find.c
@@ -1,5 +1,7 @@
 #include "execution.h"
 #include "../cleaning/cleaning.h"
+#include <asm-generic/errno-base.h>
+#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,15 +11,23 @@
 static
 int	program_exists(char *program_path, char **result_path, int print_error)
 {
-	(void)print_error;
+	int	exit_code;
+
 	if (access(program_path, X_OK | F_OK) == 0)
 	{
 		*result_path = program_path;
 		return (1);
 	}
-	//if (print_error == TRUE)
-	//	ft_printf_fd(STDERR_FILENO, "minishell: %s: %s\n", program_path, strerror(errno));
-	return (0);
+	exit_code = 0;
+	if (print_error == TRUE)
+	{
+		if (errno == EACCES)
+			exit_code = 126;
+		if (errno == ENOENT)
+			exit_code = 127;
+		ft_printf_fd(STDERR_FILENO, "minishell: %s: %s\n", program_path, strerror(errno));
+	}
+	return (exit_code);
 }
 
 static
@@ -46,29 +56,29 @@ int	program_path_get(const char *program_name, char **path, char **result)
 	return (free(tmp), 0);
 }
 
-static
-int	program_path_find(char *program_name, char **path, char **result)
-{
+//static
+//int	program_path_find(char *program_name, char **path, char **result)
+//{
+//
+//	if (program_name == NULL)
+//		return (-1);
+//	if (ft_strchr(program_name, '/') != NULL)
+//	{
+//		if (program_exists(program_name, result, TRUE) == 1)
+//			return (3);
+//		return (2);
+//	}
+//	return (program_path_get(program_name, path, result));
+//}
 
-	if (program_name == NULL)
-		return (-1);
-	if (ft_strchr(program_name, '/') != NULL)
-	{
-		if (program_exists(program_name, result, TRUE) == 1)
-			return (3);
-		return (2);
-	}
-	return (program_path_get(program_name, path, result));
-}
-
-int	command_exists(t_command *command, char **command_path, char **envp)
+int	program_path_find(t_command *command, char **command_path, char **envp)
 {
 	char	**path_env;
 	int		exit_code;
 
 	path_env = ft_split(env_get("PATH", envp), ':');
 	*command_path = NULL;
-	exit_code = program_path_find(command->argv[0], path_env, command_path);
+	exit_code = program_path_get(command->argv[0], path_env, command_path);
 	free_array((void **)path_env, -1, TRUE);
 	if (exit_code == ALLOC_FAILURE)
 	{
@@ -80,15 +90,24 @@ int	command_exists(t_command *command, char **command_path, char **envp)
 		ft_printf_fd(STDERR_FILENO, NOT_FOUND_ERROR, command->argv[0]);
 		return (EXIT_NOT_FOUND);
 	}
-	else if (exit_code == NO_FILE_FAILURE)
-	{
-		ft_printf_fd(STDERR_FILENO, NO_FILE_ERROR, command->argv[0]);
-		return (EXIT_NOT_FOUND);
-	}
-	else if (exit_code == 4)
-	{
-		ft_printf_fd(STDERR_FILENO, DIR_ERR, command->argv[0]);
-		return (126);
-	}
 	return (EXIT_SUCCESS);
+}
+
+int	absolute_path_check(char *command_path, char **result_path)
+{
+	int	exit_code;
+
+	exit_code = program_exists(command_path, result_path, TRUE);
+	if (exit_code == 1)
+		return (EXIT_SUCCESS);
+	return (exit_code);
+}
+
+int	command_exists(t_command *command, char **command_path, char **envp)
+{
+	if (command->argv[0] == NULL)
+		return (-1);
+	if (ft_strchr(command->argv[0], '/') != NULL)
+		return (absolute_path_check(command->argv[0], command_path));
+	return(program_path_find(command, command_path, envp));
 }
